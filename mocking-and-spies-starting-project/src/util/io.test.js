07095d92afe547cf7331e963421a7d64efbca8df
cjhines <christopher.hines@hybridheroes.de>
2024-05-07 11:03:02 +0200

    chore: lesson 67

--- mocking-and-spies-starting-project/src/util/io.test.js
+++ mocking-and-spies-starting-project/src/util/io.test.js
@@ -1,9 +1,22 @@
-import writeData from './io';
+import writeData from "./io";
+import { promises as fs } from "fs";
 
-it('should execute the writeFile method', async () => {
-  const testData = 'test data';
-  const testFileName = 'test.txt';
+jest.mock("fs", () => ({
+  promises: {
+    writeFile: jest.fn(),
+  },
+}));
+
+jest.mock("path", () => ({
+  join: (...args) => {
+    return args[args.length - 1];
+  },
+}));
+
+it("should execute the writeFile method", async () => {
+  const testData = "test data";
+  const testFileName = "test.txt";
 
   const result = await writeData(testData, testFileName);
-  expect(result).toBeUndefined();
+  expect(fs.writeFile).toBeCalledWith(testFileName, testData);
 });
