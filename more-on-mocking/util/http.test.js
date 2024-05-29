d264de0fc1dd3305d2e67fd26ea9fa51e9469d62
cjhines <christopher.hines@hybridheroes.de>
2024-05-29 12:50:40 +0200

    lesson 78

--- more-on-mocking/util/http.test.js
+++ more-on-mocking/util/http.test.js
@@ -1,8 +1,11 @@
 import { sendDataRequest } from "./http";
 
-const testResponseData = { testKey: 'testData'};
-const testFetch = jest.fn((url, option) => {
-  return new Promise((resolve) => {
+const testResponseData = { testKey: "testData" };
+const testFetch = jest.fn((url, options) => {
+  return new Promise((resolve, reject) => {
+    if (typeof options.body !== "string") {
+      reject("Body is not a string");
+    }
     const testResponse = {
       ok: true,
       json() {
@@ -18,8 +21,23 @@ const testFetch = jest.fn((url, option) => {
 global.fetch = testFetch;
 
 describe("sendDataRequest", () => {
-  it("should return any available response data", async  () => {
+  it("should return any available response data", async () => {
     const testData = { testData: "testData" };
     await expect(sendDataRequest(testData)).resolves.toBe(testResponseData);
+  });
+
+  it("should convert the provided data to JSON before sending the request", async () => {
+    const testData = { testData: "testData" };
+    let errorMessage;
+
+    try {
+      await sendDataRequest(testData);
+    } catch (error) {
+      errorMessage = error;
+      expect(error).toBeUndefined();
+    }
+
+    expect(errorMessage).not.toBe("string");
+
   });
 });
