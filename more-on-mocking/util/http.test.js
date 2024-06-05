c726e6a7cc1c21e03b008d991a39086b5a57bef3
cjhines <christopher.hines@hybridheroes.de>
2024-06-05 11:22:24 +0200

    lesson 80

--- more-on-mocking/util/http.test.js
+++ more-on-mocking/util/http.test.js
@@ -1,3 +1,4 @@
+import { HttpError } from "./errors";
 import { sendDataRequest } from "./http";
 
 const testResponseData = { testKey: "testData" };
@@ -38,6 +39,24 @@ describe("sendDataRequest", () => {
     }
 
     expect(errorMessage).not.toBe("string");
+  });
 
+  it("should throw an http error if the response is not ok", async () => {
+    testFetch.mockImplementationOnce(() => {
+      return new Promise((resolve) => {
+        const testResponse = {
+          ok: false,
+          json() {
+            return new Promise((resolve) => {
+              resolve(testResponseData);
+            });
+          },
+        };
+        resolve(testResponse);
+      });
+    });
+    const testData = { key: "test" };
+
+    return expect(sendDataRequest(testData)).rejects.toBeInstanceOf(HttpError )
   });
 });
