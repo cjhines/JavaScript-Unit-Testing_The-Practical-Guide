5119365d818f35ecca5746b432978c66cd0b2ca3
cjhines <christopher.hines@hybridheroes.de>
2024-06-05 13:26:54 +0200

    lesson 89

--- more-on-mocking/util/dom.test.js
+++ more-on-mocking/util/dom.test.js
@@ -2,35 +2,43 @@
  * @jest-environment jsdom
  */
 
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 
-import { showError } from './dom';
+import { showError } from "./dom";
 
-const htmlDocPath = path.join(process.cwd(), 'index.html');
-const htmlDocumentContent = fs.readFileSync(htmlDocPath, 'utf-8').toString();
+const htmlDocPath = path.join(process.cwd(), "index.html");
+const htmlDocumentContent = fs.readFileSync(htmlDocPath, "utf-8").toString();
 
 beforeEach(() => {
   document.body.innerHTML = htmlDocumentContent;
 });
 
-describe('showError', () => {
+describe("showError", () => {
   it('should add error text to the id="errors" element', () => {
-    showError('Test');
+    showError("Test");
 
-    const errorsElement = document.getElementById('errors');
-    const errorMessageElement = errorsElement.querySelector('p');
+    const errorsElement = document.getElementById("errors");
+    const errorMessageElement = errorsElement.querySelector("p");
 
     expect(errorMessageElement.textContent).not.toBeNull();
   });
 
-  it('should not contain an error initially', () => {
-    const errorsElement = document.getElementById('errors');
-    const errorMessageElement = errorsElement.querySelector('p');
+  it("should not contain an error initially", () => {
+    const errorsElement = document.getElementById("errors");
+    const errorMessageElement = errorsElement.querySelector("p");
 
     expect(errorMessageElement).toBeNull();
   });
 
+  it("should output the provided message in the error paragraph", () => {
+    const testErrorMessage = "Test Error Message";
+
+    showError(testErrorMessage);
+
+    const errorsElement = document.getElementById("errors");
+    const errorMessageElement = errorsElement.querySelector("p");
+
+    expect(errorMessageElement.textContent).toBe(testErrorMessage);
+  });
 });
-
-
